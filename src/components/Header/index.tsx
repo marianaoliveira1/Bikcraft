b75db1d1867b066bd65c7a2f760cf224f5dec3f2
Mariana Oliveira <marianaoliveirafernandes@alu.ufc.br>
2023-02-02 14:30:05 -0300

    header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,26 @@
-import { HeaderContainer } from "./styles";
+import { Link } from "react-router-dom";
 
-import imgLogo from '../../assets/bikcraft.svg'
+import imgLogo from '../../assets/bikcraft.svg';
+
+import styles from './header.module.css';
 
 export function Header() {
     return(
-        <HeaderContainer>
-            <img src={imgLogo} alt="Bikcraf" />
-        </HeaderContainer>
+        <header className={styles.headerbg}>
+            <div className={styles.header}>
+
+                <Link to="/">
+                    <img src={imgLogo} alt="Bikcraf" />
+                </Link>
+
+                <nav aria-label="primaria">
+                    <ul className={styles.headermenu}>
+                        <li><a href="/bicicletas">Bicicletas</a></li>
+                        <li><a href="/contato">Contatos</a></li>
+                        <li><a href="/seguros">Seguros</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
     )
 }
